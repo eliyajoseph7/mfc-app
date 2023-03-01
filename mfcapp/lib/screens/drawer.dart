3b7c67a9b570ed66d0f0076362771c7506bbcfd7
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-03-01 15:49:27 +0300

    first commit

---
+++ mfcapp/lib/screens/drawer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class MyDrawer extends StatefulWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
