49e62bd0f9e8f35260dba8200302b93c91068da4
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-03-01 15:49:08 +0300

    modified

--- mfcapp/lib/screens/home.dart
+++ mfcapp/lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:mfcapp/screens/drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,46 +11,70 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: ListView(
-          children: [
-            Stack(
+    var topContainerHeight = MediaQuery.of(context).size.height * 0.25;
+    var listHeight = MediaQuery.of(context).size.height - (topContainerHeight + 28);
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: Stack(
               children: [
                 Container(
-                  height: MediaQuery.of(context).size.height * 0.2,
+                  height: topContainerHeight,
                   decoration: BoxDecoration(color: Colors.grey[100]),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'Sample Text',
-                            style: TextStyle(
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
-                              // fontFamily: 'OpenSans'
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                FittedBox(
+                                  fit: BoxFit.fitHeight,
+                                  child: Text(
+                                    'Sample',
+                                    style: TextStyle(
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.bold,
+                                      // fontFamily: 'OpenSans'
+                                    ),
+                                  ),
+                                ),
+                                FittedBox(
+                                  fit: BoxFit.fitHeight,
+                                  child: Text(
+                                    'Sample Text',
+                                    style: TextStyle(
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.bold,
+                                      // fontFamily: 'OpenSans'
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: MediaQuery.of(context).size.height * 2,
-                          // width: 200,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: ExactAssetImage('assets/images/dove.png'),
+                          Container(
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width * 0.35,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: ExactAssetImage('assets/images/dove.png'),
+                              ),
                             ),
+                            // child: Image(
+                            //   image: NetworkImage(
+                            //       'https://www.nicepng.com/png/detail/113-1136930_holy-spirit-dove-png-bible-with-dove-png.png'),
+                            // ),
                           ),
-                          // child: Image(
-                          //   image: NetworkImage(
-                          //       'https://www.nicepng.com/png/detail/113-1136930_holy-spirit-dove-png-bible-with-dove-png.png'),
-                          // ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
                 const Positioned(
@@ -63,9 +86,14 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: listHeight,
+            child: ListView(),
+          )
+        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
