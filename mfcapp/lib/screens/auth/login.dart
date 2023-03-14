260e9992f06119ec6c1818912ac0888393cdb41a
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-03-14 16:52:37 +0300

    layout modified

--- mfcapp/lib/screens/auth/login.dart
+++ mfcapp/lib/screens/auth/login.dart
@@ -1,59 +1,189 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:proste_bezier_curve/proste_bezier_curve.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscurText = true;
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.45,
+    var screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Container(
+            color: Colors.grey.shade200,
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.45,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.55,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 0,
+                  child: ClipPath(
+                    clipper: ProsteThirdOrderBezierCurve(
+                      position: ClipPosition.bottom,
+                      list: [
+                        ThirdOrderBezierCurveSection(
+                          p1: const Offset(0, 100),
+                          p2: const Offset(0, 200),
+                          p3: Offset(screenWidth / 2, 100),
+                          p4: Offset(screenWidth / 2, 200),
+                        ),
+                        ThirdOrderBezierCurveSection(
+                          p1: Offset(screenWidth / 2, 200),
+                          p2: Offset(screenWidth / 2, 100),
+                          p3: Offset(screenWidth, 200),
+                          p4: Offset(screenWidth, 100),
+                        ),
+                      ],
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.295,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(25),
+                            bottomRight: Radius.circular(25),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.shade500,
+                                blurRadius: 2,
+                                spreadRadius: 3)
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Flexible(
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                  // fontFamily: 'LobsterTwo',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 35),
+                            ),
+                          ),
+                          Flexible(child: Text('&'),),
+                          Flexible(child: Padding(
+                            padding: EdgeInsets.only(bottom: 8.0),
+                            child: Text('Chart with us'),
+                          ),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.28,
+                  left: 18,
+                  right: 18,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.45,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white,
+                              spreadRadius: 1,
+                              blurRadius: 1)
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Form(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: 'Enter Username',
+                                    label: Text('Username/ Phone'),
+                                    icon: Icon(Icons.account_circle)),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: TextFormField(
+                                obscureText: _obscurText,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter Passwords',
+                                  label: const Text('Passwords'),
+                                  icon: const Icon(Icons.password),
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _obscurText = !_obscurText;
+                                      });
+                                    },
+                                    icon: _obscurText
+                                        ? const Icon(Icons.visibility_off)
+                                        : const Icon(Icons.visibility),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 38.0, top: 40),
+                              child: ElevatedButton(
+                                onPressed: null,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: const [
+                                          Padding(
+                                            padding: EdgeInsets.only(right: 8.0),
+                                            child: Icon(Icons.login),
+                                          ),
+                                          Text('Login'),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.55,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                // borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25),),
-              ),
-            ),
-          ],
-        ),
-        Positioned(
-          top: 0,
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.55,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25),
-              ),
-            ),
-            child: const Center(child: Text('hdhdh')),
           ),
-        ),
-        Positioned(
-          top: MediaQuery.of(context).size.height * 0.3,
-          left: 18,
-          right: 18,
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.45,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(25),
-              boxShadow: const [
-               BoxShadow(color: Colors.white, spreadRadius: 1, blurRadius: 1)
-            ]),
-            child: Form(child: Column(children: [],),),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
