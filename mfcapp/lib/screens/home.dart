8f5526ebd8f2e57d9cd03d488662748b93c2c43c
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-03-21 15:04:13 +0300

    layout modified

--- mfcapp/lib/screens/home.dart
+++ mfcapp/lib/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart' hide Badge, ModalBottomSheetRoute;
 import 'package:mfcapp/widgets/home/manna.dart';
 import 'package:mfcapp/widgets/home/schedule.dart';
 import 'package:mfcapp/widgets/home/top_menu.dart';
+import 'package:mfcapp/widgets/home/top_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,63 +16,129 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var topContainerHeight = MediaQuery.of(context).size.height * 0.25;
+    var topContainerHeight = MediaQuery.of(context).size.height * 0.27;
     return Scaffold(
-      backgroundColor: Color(0xFFFFFFFF),
+      backgroundColor: Color(0xFFDFDDCE),
+      // backgroundColor: Color(0xFF000000),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFDFDDCE),
+        // backgroundColor: Color(0xff091827),
+        // title: FittedBox(child: Text('Welcome MFC', style: TextStyle(color: Colors.white70, fontSize: 35, fontWeight: FontWeight.w700),)),
+        elevation: 0,
+        leadingWidth: 100,
+        leading: Container(
+          height: 70,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/mfclogo_square.jpeg'),
+                  fit: BoxFit.contain),
+              // color: Color(0xFFffffff),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              ),),
+        ),
+      ),
       body: Column(
         children: [
           Stack(
             children: [
               SizedBox(
-                height: topContainerHeight,
+                height: topContainerHeight * 0.5,
                 child: Container(
-                  decoration: BoxDecoration(color: Color(0xFFFFFFFF)),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                  decoration: BoxDecoration(
+                    // image: DecorationImage(
+                    //     image: AssetImage('assets/images/mfclogo_square.jpeg'),
+                    //     fit: BoxFit.contain),
+                    color: Color(0xFF091827),
+                  ),
+                  // child: Row(
+                  //   crossAxisAlignment: CrossAxisAlignment.center,
+                  //   children: [
+                  //     Expanded(
+                  //       child: Column(
+                  //         crossAxisAlignment: CrossAxisAlignment.center,
+                  //         mainAxisAlignment: MainAxisAlignment.center,
+                  //         children: const [
+                  //           FittedBox(
+                  //             fit: BoxFit.fitHeight,
+                  //             child: Padding(
+                  //               padding: EdgeInsets.symmetric(horizontal: 3.0),
+                  //               child: Text(
+                  //                 'Holy Spirit',
+                  //                 style: TextStyle(
+                  //                     fontSize: 50,
+                  //                     fontWeight: FontWeight.bold,
+                  //                     fontFamily: 'LobsterTwo'),
+                  //               ),
+                  //             ),
+                  //           ),
+                  //           FittedBox(
+                  //             fit: BoxFit.fitHeight,
+                  //             child: Padding(
+                  //               padding: EdgeInsets.symmetric(horizontal: 3.0),
+                  //               child: Text(
+                  //                 'Breath in Me',
+                  //                 style: TextStyle(
+                  //                     fontSize: 50,
+                  //                     fontWeight: FontWeight.bold,
+                  //                     fontFamily: 'DancingScript'),
+                  //               ),
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ),
+                  //     Container(
+                  //       height: MediaQuery.of(context).size.height,
+                  //       width: MediaQuery.of(context).size.width * 0.35,
+                  //       decoration: const BoxDecoration(
+                  //         image: DecorationImage(
+                  //           image: ExactAssetImage('assets/images/dove.png'),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      // SizedBox(
+                      //   height: topContainerHeight * 0.45,
+                      //   child: Row(
+                      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //     children: [
+                      //       Padding(
+                      //         padding: const EdgeInsets.only(left: 8.0),
+                      //         child: Text(
+                      //           'Welcome',
+                      //           style: TextStyle(
+                      //             color: Colors.white70,
+                      //             fontSize: 25,
+                      //             fontWeight: FontWeight.w700,
+                      //           ),
+                      //         ),
+                      //       ),
+                      //       // Container(
+                      //       //   width: MediaQuery.of(context).size.width * 0.4,
+                      //       //   decoration: BoxDecoration(
+                      //       //     image: DecorationImage(
+                      //       //       image: AssetImage('assets/images/mfclogo.png'),
+                      //       //     ),
+                      //       //     color: Colors.black
+                      //       //   ),
+                      //       // ),
+                      //     ],
+                      //   ),
+                      // ),
                       Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            FittedBox(
-                              fit: BoxFit.fitHeight,
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 3.0),
-                                child: Text(
-                                  'Holy Spirit',
-                                  style: TextStyle(
-                                      fontSize: 50,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'LobsterTwo'),
-                                ),
-                              ),
-                            ),
-                            FittedBox(
-                              fit: BoxFit.fitHeight,
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 3.0),
-                                child: Text(
-                                  'Breath in Me',
-                                  style: TextStyle(
-                                      fontSize: 50,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'DancingScript'),
-                                ),
-                              ),
-                            ),
-                          ],
+                        child: Container(
+                          color: Color(0xFFDFDDCE),
+                          child: TopScreenList(),
                         ),
                       ),
-                      Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width * 0.35,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: ExactAssetImage('assets/images/dove.png'),
-                          ),
-                        ),
-                      ),
+                      // Container(height: 5, color: Color(0xFFDFDDCE),)
                     ],
                   ),
                 ),
@@ -91,6 +158,7 @@ class _HomePageState extends State<HomePage> {
                     topRight: Radius.circular(20),
                   ),
                   color: Color(0xFFECECEF),
+                  // color: Colors.white70,
                 ),
                 child: ListView(
                   physics: BouncingScrollPhysics(),
