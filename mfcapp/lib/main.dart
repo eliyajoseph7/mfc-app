c787a7f70d4476d85e9e65164390f87edac76db2
eliyajoseph7 <exaveryeliya20@gmail.com>
2023-03-21 19:34:39 +0300

    splash screen and launcher icon added

--- mfcapp/lib/main.dart
+++ mfcapp/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mfcapp/providers/auth.dart';
 import 'package:mfcapp/screens/index.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
